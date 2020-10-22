be379f21b287a0e5bae65db134641a6843b63081
SashaJson <sashamiller666@gmail.com>
2020-10-23 02:39:31 +0300

    added db

--- django3Try/tryDjango/main/models.py
+++ django3Try/tryDjango/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('title', max_length=50)
+    anons = models.CharField('anons', max_length=50)
+    full_text = models.TextField('body article')
+    date = models.DateTimeField('Date push article')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'piece of news'
+        verbose_name_plural = 'news'
